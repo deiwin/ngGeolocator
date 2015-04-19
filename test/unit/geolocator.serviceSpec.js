cd9bf29add500e8ce95af457b166743c5f903546
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2015-04-19 23:19:42 +0300

    add test for when geolocation isn't supported

--- test/unit/geolocator.serviceSpec.js
+++ test/unit/geolocator.serviceSpec.js
@@ -3,11 +3,18 @@ describe('ngGeolocator', function() {
   beforeEach(module('ngGeolocator'));
 
   describe('service', function() {
-    var service, $window;
-    beforeEach(inject(function(ngGeolocator, _$window_) {
+    var service, $window, $rootScope;
+    beforeEach(inject(function(ngGeolocator, _$window_, _$rootScope_) {
       service = ngGeolocator;
       $window = _$window_;
+      $rootScope = _$rootScope_;
       $window.document.body.appendChild = jasmine.createSpy('appendChild');
+      $window.google = {
+        maps: {
+          LatLng: jasmine.createSpy('maps.LatLng'),
+          InfoWindow: jasmine.createSpy('maps.InfoWindow'),
+        },
+      };
     }));
 
     describe('create', function() {
@@ -23,9 +30,24 @@ describe('ngGeolocator', function() {
           expect(appendChild.calls.first().args[0].src).toMatch('maps.googleapis.com/maps/api/js');
         });
 
-        it ('should include the key, if added', function() {
+        it('should include the key, if added', function() {
           service.create('', 'a-test-key');
           expect(appendChild.calls.first().args[0].src).toMatch('key=a-test-key');
+        });
+      });
+
+      describe('getting the user\'s location', function() {
+        describe('with no geolocation service available', function() {
+          beforeEach(function() {
+            delete $window.navigator.geolocation;
+          });
+
+          it('should fail', function(done) {
+            service.create().catch(function() {
+              done();
+            });
+            $rootScope.$apply();
+          }, 500);
         });
       });
     });
